83d43c156f0e7a74bf29c1ba5f7529f7b31dfe51
Michael <michael.krasheninikov@gmail.com>
2024-04-20 10:31:00 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using system;
+namespace PractISRPO
+{
+    class programm
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            Console.WriteLine("Введите первое число");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сумма числе лавна" + (x + y));
+        }
+    }
+}
